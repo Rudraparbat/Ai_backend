3f0f59997824115d59e9ee145c118e11be3f95fc
anasuya100 <royanasuya346@gmail.com>
2025-03-08 01:22:41 +0530

    added origins

--- ai_api/settings.py
+++ ai_api/settings.py
@@ -47,11 +47,13 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
     "http://localhost:5174",
+    "https://aiapi-vdez.onrender.com"
 
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",  
     "http://localhost:5174",
+    "https://aiapi-vdez.onrender.com"
 
 ]
 
